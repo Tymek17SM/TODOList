511ccefd26235f5f693851638d7c82ecb8b8b5d8
Tymoteusz Nowacki <tymrud99@gmail.com>
2023-06-28 10:19:22 +0200

    Domain - Update IToDoTaskRepository

--- Domain/Repositories/IToDoTaskRepository.cs
+++ Domain/Repositories/IToDoTaskRepository.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entitis;
+﻿using Domain.Entities;
+using Domain.ValueObjects.ToDoTask;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,7 @@ namespace Domain.Interfaces
 {
     public interface IToDoTaskRepository
     {
-        Task<IEnumerable<ToDoTask>> GetAllAsynch(int pageNumber, int pageSize, string sortField, bool ascending, string filterBy);
-        Task<int> GetAllCountAsynch(string filterBy);
-        Task<ToDoTask> GetByIdAsynch(int id);
+        Task<ToDoTask> GetByIdAsynch(ToDoTaskId id);
         Task<ToDoTask> AddAsynch(ToDoTask task);
         Task UpdateAsynch(ToDoTask task);
         Task DeleteAsynch(ToDoTask task);
